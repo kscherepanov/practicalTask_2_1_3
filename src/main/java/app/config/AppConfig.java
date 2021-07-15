06101281b9f8a7a5b65e50c339bd5aa3e67923af
Konstantin Cherepanov <k.s.cherepanov@gmail.com>
2021-07-15 16:29:18 +0300

    practicalTask_2_1_3 solution

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,15 @@
+package app.config;
+
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+//    @Bean(name = "dog")
+//    public Dog getDog() {
+//        return new Dog();
+//    }
+}
